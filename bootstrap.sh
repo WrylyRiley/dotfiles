90ee0337aae125ea6de578bd1834599c89823122
Riley Bauer <wryr1ley@gmail.com>
2022-06-15 17:27:16 -0400

    dock icons

--- bootstrap.sh
+++ bootstrap.sh
@@ -149,20 +149,20 @@ inform "Setting dock shortcuts"
 dockutil --remove all
 dockutil --add '~/Downloads' --view list --display folder
 dockutil --add '~/programming' --view list --display folder
+dockutil --add '/Applications/Privileges.app'
 dockutil --add '/Applications/Visual Studio Code.app'
-dockutil --add '/Applications/Android Studio.app'
 dockutil --add '/Applications/iTerm.app'
 dockutil --add '/Applications/Vivaldi.app'
-dockutil --add '/Applications/Firefox Developer Edition.app'
 dockutil --add '/Applications/Insomnia.app'
-dockutil --add '/Applications/pgAdmin 4.app'
 dockutil --add '/Applications/Flipper.app'
+dockutil --add '/Applications/Beekeeper Studio.app'
 dockutil --add '/Applications/Slack.app'
-dockutil --add '/Applications/Telegram Desktop.app'
-dockutil --add '/Applications/TIDAL.app'
-dockutil --add '/Applications/1Password 7.app'
+dockutil --add '/Applications/Spotify.app'
 dockutil --add '/Applications/zoom.us.app'
-dockutil --add '/System/Applications/System Preferences.app'
+dockutil --add '/Applications/Telegram Desktop.app'
+dockutil --add '/Applications/1Password 7.app'
+dockutil --add '/Applications/Android Studio.app'
+dockutil --add '/Applications/XCode.app'
 
 # Mouse Settings
 inform "Updating trackpad settings"
